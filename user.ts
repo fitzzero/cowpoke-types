32f388afbeb95da4b5d5fc954368b6f389bda7da
logan <loganfarseer@gmail.com>
2024-07-08 21:30:24 -0700

    undo

---
+++ user.ts
@@ -0,0 +1,18 @@
+import { _BaseProps } from "./_base";
+
+export interface UserBasic extends _BaseProps {
+  name: string;
+  image: string;
+}
+
+export interface UserProps extends UserBasic {
+  accessId?: string;
+  email: string;
+  emailVerified: boolean | null;
+}
+
+export interface SessionProps extends _BaseProps {
+  expires: Date;
+  sessionToken: string;
+  userId: string;
+}

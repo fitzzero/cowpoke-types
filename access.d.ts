16d6d8e3decc2cd7412c9850b7343da3fa5f0a08
logan <loganfarseer@gmail.com>
2024-07-10 22:43:12 -0700

    'sync'

--- access.d.ts
+++ access.d.ts
@@ -1,37 +1,38 @@
-import { Id, Nullish, _BaseProps } from "./_base";
-import { EntityResponse } from "./entity";
+import { Id, Nullish, _BaseProps } from './_base'
+import { AccessLevels } from './common'
+import { EntityResponse } from './entity'
 export interface AceProps extends _BaseProps {
-  createdAt: number;
-  entityKind: string;
-  entityId: string;
-  0: string[];
-  1: string[];
-  2: string[];
-  3: string[];
-  4: string[];
+  createdAt: number
+  entityKind: string
+  entityId: string
+  0: string[]
+  1: string[]
+  2: string[]
+  3: string[]
+  4: string[]
 }
 
 export type AceLookup = (
   entityKind: string,
   entityId: string,
-  defaultAccess?: Record<number, string[]>
-) => Promise<AceProps | Nullish>;
+  defaultAccess?: Record<AccessLevels, string[]>
+) => Promise<AceProps | Nullish>
 
 export interface AccessProps extends _BaseProps {
-  userId: string;
-  scopes: Scopes;
+  userId: string
+  scopes: Scopes
 }
 
-export type Scopes = Record<string, number>;
+export type Scopes = Record<string, AccessLevels>
 
 export interface CheckAccessProps {
-  access?: AccessProps;
-  entity?: Id;
-  reqLevel: number;
-  response: EntityResponse<any>;
+  access?: AccessProps
+  entity?: Id
+  reqLevel: AccessLevels
+  response: EntityResponse<any>
 }
 
 export type FindHighestAccess = (props: {
-  access?: AccessProps;
-  entity?: Id;
-}) => Promise<number>;
+  access?: AccessProps
+  entity?: Id
+}) => Promise<AccessLevels>
